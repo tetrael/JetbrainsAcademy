5e80bfefdcbc84ef8ecb5a018df277a178b85aac
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-12 16:26:41 +0000

    Stage 3 completed

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -1,6 +1,8 @@
 # Write your code here
-SECRET_WORD = "python"
+import random
 
+SECRETS = ["python", "java", "kotlin", "javascript"]
+SECRET_WORD = SECRETS[random.randint(0, len(SECRETS)-1)]
 
 if input("Guess the word:") == SECRET_WORD:
     print("You survived!")
