0dea9a259d862ddfff01949e29e0f3e35f81443a
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-14 20:23:22 +0000

    Stage 6 working solution, but this time I little improve code and make it more readable and simple

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -1,30 +1,25 @@
-# Write your code here
 import random
 
 print("H A N G M A N")
 attempts = 8
 words_pool = ["python", "java", "kotlin", "javascript"]
 chosen_word = random.choice(words_pool)
-game_word = str(''.join(["-" for x in chosen_word[::]]))
+guessed_letters = set()
 
-while game_word != chosen_word:
+while attempts > 0:
+    game_word = ''.join(letter if letter in guessed_letters else "-" for letter in chosen_word)
+    print('\n' + game_word)
+    letter = input("Input a letter: ")
+
+    if letter not in chosen_word or letter in game_word:
+        print("No improvements" if letter in game_word else "That letter doesn't appear in the word")
+        attempts -= 1
+    else:
+        guessed_letters.add(letter)
+
     if attempts == 0:
         print("You lost!")
         break
-    print('\n' + game_word)
-    letter = input("Input a letter: ")
-    if letter in chosen_word:
-        if letter in game_word:
-            print("No improvements")
-            attempts -= 1
-        for x in range(len(chosen_word)):
-            if chosen_word[x] == letter:
-                game_word = list(game_word)
-                game_word[x] = letter
-                game_word = str(''.join(game_word))
-    elif letter not in chosen_word:
-        print("That letter doesn't appear in the word")
-        attempts -= 1
 
 else:
     print("You survived!")
