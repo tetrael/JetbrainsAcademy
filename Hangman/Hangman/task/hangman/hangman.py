9fede30315d33add739fa8e431e2553ef925b98c
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-14 09:20:06 +0000

    Stage 4 completed

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -3,8 +3,11 @@ import random
 
 SECRETS = ["python", "java", "kotlin", "javascript"]
 SECRET_WORD = SECRETS[random.randint(0, len(SECRETS)-1)]
+hint = SECRET_WORD[:3] + str(''.join(["-" for x in SECRET_WORD[3:]]))
 
-if input("Guess the word:") == SECRET_WORD:
+if input("Guess the word {}:".format(hint)) == SECRET_WORD:
     print("You survived!")
 else:
     print("You lost!")
+
+
