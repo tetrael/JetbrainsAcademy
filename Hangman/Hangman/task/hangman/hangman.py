21ae063cc1c8450d73f6ff8a9f2c8a08a6d56cbd
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-14 19:21:44 +0000

    Stage 6 working solution

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -7,15 +7,24 @@ words_pool = ["python", "java", "kotlin", "javascript"]
 chosen_word = random.choice(words_pool)
 game_word = str(''.join(["-" for x in chosen_word[::]]))
 
-while attempts > 0:
+while game_word != chosen_word:
+    if attempts == 0:
+        print("You lost!")
+        break
     print('\n' + game_word)
     letter = input("Input a letter: ")
-    game_word = ''.join(letter if letter in game_word else "-" for letter in chosen_word)
-    if letter not in chosen_word:
+    if letter in chosen_word:
+        if letter in game_word:
+            print("No improvements")
+            attempts -= 1
+        for x in range(len(chosen_word)):
+            if chosen_word[x] == letter:
+                game_word = list(game_word)
+                game_word[x] = letter
+                game_word = str(''.join(game_word))
+    elif letter not in chosen_word:
         print("That letter doesn't appear in the word")
         attempts -= 1
-    elif letter not in game_word:
-        print("No improvement")
-        attempts -= 1
+
 else:
-    print("You survived!" if game_word == chosen_word else "You lost!")
+    print("You survived!")
