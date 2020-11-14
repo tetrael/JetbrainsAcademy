a23e5a092cec5e37aa8ad4bac11243ec758c9b3c
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-14 17:52:35 +0000

    first working solution for stage 5

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -1,27 +1,16 @@
 # Write your code here
 import random
 
-# SECRETS = ["python", "java", "kotlin", "javascript"]
-# SECRET_WORD = SECRETS[random.randint(0, len(SECRETS)-1)]
-# hint = SECRET_WORD[:3] + str(''.join(["-" for x in SECRET_WORD[3:]]))
-#
-# if input("Guess the word {}:".format(hint)) == SECRET_WORD:
-#     print("You survived!")
-# else:
-#     print("You lost!")
-
-print("H A N G M A N" + '\n')
+print("H A N G M A N")
 attempts = 8
 words_pool = ["python", "java", "kotlin", "javascript"]
-chosen_word = words_pool[random.randint(0, len(words_pool)-1)]
+chosen_word = random.choice(words_pool)
 game_word = str(''.join(["-" for x in chosen_word[::]]))
 
-while game_word != chosen_word and attempts > 0:
-    guested_letters = list()
-    print(game_word)
+while attempts > 0:
+    print('\n' + game_word)
     letter = input("Input a letter: ")
-    if letter in chosen_word and letter not in guested_letters:
-        guested_letters.append(letter)
+    if letter in chosen_word:
         for x in range(len(chosen_word)):
             if chosen_word[x] == letter:
                 game_word = list(game_word)
@@ -29,7 +18,8 @@ while game_word != chosen_word and attempts > 0:
                 game_word = str(''.join(game_word))
     else:
         print("That letter doesn't appear in the word")
-        attempts -= 1
+
+    attempts -= 1
 else:
-    print("Thanks for playing!");
+    print("\nThanks for playing!");
     print("We'll see how well you did in the next stage")
