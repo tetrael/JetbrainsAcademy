8dddbb1641526b1a154e82cb12ea5d8de136e5eb
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-14 21:17:29 +0000

    Stage 7 solution complete and working.
    Rise and shine

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -10,16 +10,26 @@ while attempts > 0:
     game_word = ''.join(letter if letter in guessed_letters else "-" for letter in chosen_word)
     print('\n' + game_word)
     letter = input("Input a letter: ")
+    if len(letter) != 1:
+        print("You should input a single letter")
+        continue
+    if not letter.islower() or not letter.isalpha():
+        print("Please enter a lowercase English letter")
+        continue
 
-    if letter not in chosen_word or letter in game_word:
-        print("No improvements" if letter in game_word else "That letter doesn't appear in the word")
-        attempts -= 1
-    else:
+    if letter not in chosen_word and letter not in guessed_letters:
+        print("That letter doesn't appear in the word")
         guessed_letters.add(letter)
+        attempts -= 1
+    elif letter not in guessed_letters:
+        guessed_letters.add(letter)
+    else:
+        print("You've already guessed this letter")
 
     if attempts == 0:
         print("You lost!")
         break
 
 else:
+    print("You guessed the word {}!".format(game_word))
     print("You survived!")
