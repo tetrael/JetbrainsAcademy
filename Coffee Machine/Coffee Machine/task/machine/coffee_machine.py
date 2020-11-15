98b1e9ffab4faf5e9d20a49858e480cc0e1a369f
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-15 12:35:21 +0000

    Stage 2 - This solution works for the validator, but I'm sure that the last one is correct too.
    Ok, let's move on

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -2,12 +2,9 @@ water_per_cup = int(200)
 milk_per_cup = int(50)
 coffee_beans_per_cup = int(15)
 
-coffee_cups = int(input("Write how many cups of coffee you will need:" + '\n'))
-print("""For {} cups of coffee you will need:
-{} ml of water
-{} ml of milk
-{} g of coffee beans
-""".format(coffee_cups,
-           coffee_cups * water_per_cup,
-           coffee_cups * milk_per_cup,
-           coffee_cups * coffee_beans_per_cup))
+print("Write how many cups of coffee you will need:")
+coffee_cups = int(input())
+print("For {} cups of coffee you will need:".format(coffee_cups))
+print("{} ml of water".format(coffee_cups * water_per_cup))
+print("{} ml of milk".format(coffee_cups * milk_per_cup))
+print("{} g of coffee beans".format(coffee_cups * coffee_beans_per_cup))
