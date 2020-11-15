2d1e980f016551a51b5d860e86e0fe3356b451ca
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-15 12:31:36 +0000

    Stage 2 - I am pretty sure that solution working well, but for validator is it not. Weird, why ?

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -1,9 +1,13 @@
-# Write your code here
-print("""
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!""")
+water_per_cup = int(200)
+milk_per_cup = int(50)
+coffee_beans_per_cup = int(15)
+
+coffee_cups = int(input("Write how many cups of coffee you will need:" + '\n'))
+print("""For {} cups of coffee you will need:
+{} ml of water
+{} ml of milk
+{} g of coffee beans
+""".format(coffee_cups,
+           coffee_cups * water_per_cup,
+           coffee_cups * milk_per_cup,
+           coffee_cups * coffee_beans_per_cup))
