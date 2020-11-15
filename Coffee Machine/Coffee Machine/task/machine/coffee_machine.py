cd419e1293e4a7715b8b4df555d063d1da2de9a9
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-11-15 12:23:11 +0000

    Step 1 completed

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -1,2 +1,9 @@
 # Write your code here
-print("Hello from new GIT branch!!!")
+print("""
+Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!""")
