27d00bad183119669dd253b77083f1d0d538a604
Przemek Kozłowski <kozlowskiprzemek@gmail.com>
2020-12-08 16:40:08 +0000

    Stage 1 works correct!

--- Simple Banking System/Simple Banking System/task/banking/banking.py
+++ Simple Banking System/Simple Banking System/task/banking/banking.py
@@ -11,13 +11,13 @@ class Bank:
     def print_main_menu():
         print("1. Create an account")
         print("2. Log into account")
-        print("0. Exit")
+        print("0. Exit\n")
 
     @staticmethod
     def print_login_menu():
         print("1. Balance")
         print("2. Log out")
-        print("0. Exit")
+        print("0. Exit\n")
 
     def menu(self):
         while not self.menuexit:
@@ -31,62 +31,67 @@ class Bank:
                         self.print_login_menu()
                         menuitem = int(input())
                         if menuitem == 1:
-                            print("Balance: 0")
+                            print("Balance: 0\n")
                             pass
                         elif menuitem == 2:
-                            print("You have successfully logged out!")
+                            print("You have successfully logged out!\n")
                             break
                         elif menuitem == 0:
                             self.menuexit = True
                             print("Bye!")
                             break
                 else:
-                    print("Wrong card number or PIN!")
+                    print("Wrong card number or PIN!\n")
             elif menuitem == 0 or self.menuexit:
                 print("Bye!")
                 break
 
     def login(self):
-        account = input("Enter your card number:\n")
+        number = input("Enter your card number:\n")
         pin = input("Enter your PIN:\n")
-        if account in self.accounts:
-            return True
+
+        for account in self.accounts:
+            for card in account.cards:
+                if card.number == number and card.pin == pin:
+                    print("You have successfully logged in!\n")
+                    return True
         else:
             return False
 
 
 class BankAccount:
-    accounts = []
+    cards = []
 
     def __init__(self):
         self.card = BankCard()
-        BankAccount.accounts.append(self.card)
+        BankAccount.cards.append(self.card)
 
 
 class BankCard:
     def __init__(self):
         self.pin = self.generate_pin()
-        self.number = self.generate_number()
+        self.identifiers = self.generate_identifiers()
+        self.number = self.number()
         print("Your card has been created")
-        print("Your card number:\n" + self.card_number())
-        print("Your card PIN:\n" + self.pin)
+        print("Your card number:\n" + self.number)
+        print("Your card PIN:\n" + self.pin + "\n")
 
     @staticmethod
     def generate_pin():
         return ''.join(secrets.choice(string.digits) for i in range(4))
 
     @staticmethod
-    def generate_number():
+    def generate_identifiers():
         number = dict()
         number["IIN"] = '400000'
         number["Account Identifier"] = ''.join(secrets.choice(string.digits) for i in range(9))
         number["Checksum"] = ''.join(secrets.choice(string.digits) for i in range(1))
         return number
 
-    def card_number(self):
-        return str(self.number["IIN"] +
-                   self.number["Account Identifier"] +
-                   self.number["Checksum"])
+    def number(self):
+        return str(self.identifiers["IIN"] +
+                   self.identifiers["Account Identifier"] +
+                   self.identifiers["Checksum"])
 
 
 bank = Bank()
